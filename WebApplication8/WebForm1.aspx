94b2a2c67daea335a957d97ff7e7f61388df0a3a
renatoanavrin <renatoanavrin@gmail.com>
2016-05-21 11:59:54 -0300

    Add project files.

---
+++ WebApplication8/WebForm1.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication8.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Comunicador renato</title>
+     <!--<link type="text/css" rel="stylesheet" href="Content/bootstrap.css" />-->
+    <link href="Content/bootstrap.css" rel="stylesheet" />
+    <link href="Content/StyleSheet1.css" rel="stylesheet" />
+  </head>
+    <!--<script src="Scripts/jquery-1.10.2.min.js"></script> 
+    <script src="Scripts/bootstrap.min.js"></script> -->
+<script src="Scripts/jquery-1.10.2.js"></script>
+<script src="Scripts/bootstrap.min.js"></script>
+
+<body class="fundo-cinza">
+    <form id="form1" runat="server">
+        <div class="col-md-12 fundo-azul height120 barra" ></div>
+
+        <div class="col-md-8  height800 col-centered principal fundo-branco">
+            
+            <div class="col-md-4">
+              
+                <asp:Literal ID="Literal1" runat="server"></asp:Literal>
+            </div>
+
+            <div class="col-md-8 caixaMensagens">
+
+            </div>
+        </div>
+    <div>
+        <asp:Button ID="addUsuario" runat="server" Text="Adicionar Usuário" OnClick="addUsuario_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication8/WebForm1.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.OleDb;
+
+namespace WebApplication8
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void addUsuario_Click(object sender, EventArgs e)
+        {
+
+            OleDbConnection conn = new OleDbConnection();
+            OleDbCommand cmd = new OleDbCommand();
+            OleDbDataReader dr;
+
+            conn.ConnectionString = "Provider=Microsoft.Jet.OleDb.4.0;Data Source=C:\\Users\\Renato\\mensageiro\\mensageiro.mdb";
+            //C:\Users\Renato\mensageiro
+
+            cmd.Connection = conn;
+            //cmd.CommandText ="select a.nome from usuario as a inner join usuarioAmigos as b where a.usuario_id = b.usuario_id ";
+            cmd.CommandText = "select nome from usuario";
+
+            cmd.CommandType = CommandType.Text;
+
+            conn.Open();
+
+            dr = cmd.ExecuteReader();
+
+
+            /*
+             * while (rdr.Read())
+                {
+                    lbProdutos.Items.Add(rdr["ProductID"].ToString() +
+                    " - " + rdr["ProductName"].ToString() + " - " + rdr["UnitPrice"].ToString());
+                    nuReg++;
+                }*/
+
+            String divs = "";
+
+            while (dr.Read())
+            {
+                divs = divs + "<div class='usuario'>" + dr["nome"] +"</div>";
+            }
+
+
+ 
+
+            /*for (int x=0; x<=10;x++)
+            {
+                divs = divs + "<div class='usuario jumbotron'>usuario" +x+ " </div>";
+            }*/
+            Literal1.Text = divs;
+
+            conn.Close();
+            /*< div class="usuario">
+                    teste
+                </div>*/
+        }
+    }
+}
